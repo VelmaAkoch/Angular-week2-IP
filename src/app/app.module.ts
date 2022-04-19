95e4b843917bef779f9a0387b28e4f9dc15cac19
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-20 00:05:37 +0300

    add a new file ofthe landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandigPageComponent } from './landig-page/landig-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandigPageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
