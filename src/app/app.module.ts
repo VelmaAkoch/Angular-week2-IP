c5449a2167c3315f1be557d02fa22bb1848cfdf9
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-20 18:06:16 +0300

    add the new file on app.components of h1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandigPageComponent } from './landig-page/landig-page.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandigPageComponent,
-    LandingPageComponent
+  
   ],
   imports: [
     BrowserModule,
