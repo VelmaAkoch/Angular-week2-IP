bbfed4b206f568e89cac853b2b3812ea2cb668f4
VelmaAkoch <velmaakochi1@gmail.com>
2022-04-24 12:52:56 +0300

    add new pipes generated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule,NgForm } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     FormComponent,
     NotFoundComponent,
     NavbarComponent,
+    PipesComponent,
   
   
   ],
